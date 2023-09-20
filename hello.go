e8957298620762d906e5f210c7c309a8dbfa4df9
GaryTang <gary.tang@citi.com>
2023-09-20 16:52:58 -0500

    adding logic

--- hello.go
+++ hello.go
@@ -17,21 +17,48 @@ func main() {
 			"message": "pong",
 		})
 	})
+
 	r.GET("/calc", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"Result": calculate(3, 5),
 		})
 	})
-	r.GET("/runup/:duration", func(c *gin.Context) {
+
+	r.GET("/runup/:duration/:percentage", func(c *gin.Context) {
 		duration := c.Param("duration")
 		int1, err := strconv.Atoi(duration)
 		if err != nil {
 			fmt.Println("Error during conversion")
 			return
 		}
-		running(int1)
+		percentage := c.Param("percentage")
+		int2, err := strconv.Atoi(percentage)
+		if err != nil {
+			fmt.Println("Error during conversion")
+			return
+		}
+		runcpuload(runtime.NumCPU(), int1, int2)
+
+		str_cat := "Revving" + " " + percentage + "% " + duration + " seconds"
+
 		c.JSON(http.StatusOK, gin.H{
-			"Action": "Revving",
+			"Action": str_cat,
+		})
+	})
+
+	r.GET("/maxup/:duration", func(c *gin.Context) {
+		duration := c.Param("duration")
+		int1, err := strconv.Atoi(duration)
+		if err != nil {
+			fmt.Println("Error during conversion")
+			return
+		}
+		maxup(int1)
+
+		str_cat := "Revving" + " 100%" + " " + duration + " seconds"
+
+		c.JSON(http.StatusOK, gin.H{
+			"Action": str_cat,
 		})
 	})
 
@@ -42,7 +69,42 @@ func calculate(x int, y int) int {
 	return x + y
 }
 
-func running(x int) {
+func runcpuload(coresCount int, timeSeconds int, percentage int) {
+	runtime.GOMAXPROCS(coresCount)
+
+	// second     ,s  * 1
+	// millisecond,ms * 1000
+	// microsecond,μs * 1000 * 1000
+	// nanosecond ,ns * 1000 * 1000 * 1000
+
+	// every loop : run + sleep = 1 unit
+
+	// 1 unit = 100 ms may be the best
+	unitHundresOfMicrosecond := 1000
+	runMicrosecond := unitHundresOfMicrosecond * percentage
+	sleepMicrosecond := unitHundresOfMicrosecond*100 - runMicrosecond
+	for i := 0; i < coresCount; i++ {
+		go func() {
+			runtime.LockOSThread()
+			// endless loop
+			for {
+				begin := time.Now()
+				for {
+					// run 100%
+					if time.Now().Sub(begin) > time.Duration(runMicrosecond)*time.Microsecond {
+						break
+					}
+				}
+				// sleep
+				time.Sleep(time.Duration(sleepMicrosecond) * time.Microsecond)
+			}
+		}()
+	}
+	// how long
+	time.Sleep(time.Duration(timeSeconds) * time.Second)
+}
+
+func maxup(x int) {
 	done := make(chan int)
 
 	for i := 0; i < runtime.NumCPU(); i++ {
